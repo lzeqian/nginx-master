9738a901f9642061728ae3f969a6d3e41ed2104e
John Doe <john@sample.com>
2022-11-23 17:24:42 +0800

    init

---
+++ conf/x-waf/config.lua
@@ -0,0 +1,84 @@
+--[[
+
+Copyright (c) 2016 xsec.io
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THEq
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+
+]]
+
+-- WAF config file, enable = "on", disable = "off"
+
+local _M = {
+    -- waf status
+    config_waf_enable = "on",
+    -- log dir
+    config_log_dir = "/tmp/",
+    -- rule setting
+    config_rule_dir = "/cygdrive/d/code1/nginx-master/conf/x-waf/rules",
+    -- enable/disable white url
+    config_white_url_check = "on",
+    -- enable/disable white ip
+    config_white_ip_check = "on",
+    -- enable/disable block ip
+    config_black_ip_check = "on",
+    -- enable/disable url filtering
+    config_url_check = "on",
+    -- enalbe/disable url args filtering
+    config_url_args_check = "on",
+    -- enable/disable user agent filtering
+    config_user_agent_check = "on",
+    -- enable/disable cookie deny filtering
+    config_cookie_check = "on",
+    -- enable/disable cc filtering
+    config_cc_check = "on",
+    -- cc rate the xxx of xxx seconds
+    config_cc_rate = "10/60",
+    -- enable/disable post filtering
+    config_post_check = "on",
+    -- config waf output redirect/html/jinghuashuiyue
+    config_waf_model = "html",
+    -- if config_waf_output ,setting url
+    config_waf_redirect_url = "http://xsec.io",
+    config_expire_time = 600,
+    config_output_html = [[
+    <html>
+    <head>
+    <meta charset="UTF-8">
+    <title>MIDUN WAF</title>
+    </head>
+      <body>
+        <div>
+      <div class="table">
+        <div>
+          <div class="cell">
+            您的IP为: %s
+          </div>
+          <div class="cell">
+            欢迎在遵守白帽子道德准则的情况下进行安全测试。
+          </div>
+          <div class="cell">
+            联系方式：http://xsec.io
+          </div>
+        </div>
+      </div>
+    </div>
+      </body>
+    </html>
+    ]],
+}
+
+return _M
---
+++ conf/x-waf/hello.lua
@@ -0,0 +1,1 @@
+print(string.match("' AND (SELECT 3213 FROM (SELECT(SLEEP(1)))hlbG) ", "select.+(from|limit)"))
